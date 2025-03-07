90b8143df7d21634ba084ed7bb14589a5fd22a05
ZixianChen8 <59811246+ZixianChen8@users.noreply.github.com>
2025-03-07 13:42:54 -0500

    Database model update
    DB model still need improvement

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,10 +1,64 @@
 from django.db import models
 
 # Create your models here.
+class Participant(models.Model):
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    wechat_id = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Event(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateTimeField()
+    title = models.CharField(max_length=200, null=False, unique=True)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateField(null=False)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    location = models.TextField(null=True)
+    organizer = models.CharField(max_length=200, null=True)
+    link = models.URLField(blank=True, null=True, unique=True)
+    thumbnail_link = models.URLField(blank=True)
+    
+    participants = models.ManyToManyField(Participant, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class BgMedia(models.Model):
+    title = models.CharField(max_length=200, null=False, unique=True)
+    link = models.URLField(blank=True, null=True, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    wechat_id = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    position = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    pfp_url = models.CharField(max_length=200, blank=True, null=True, unique=False)
+    department = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class department(models.Model):
+    code = models.CharField(max_length=10, null=False)
+    manager = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True, related_name='managed_departments')  
+    members = models.ManyToManyField(Member, related_name='departments')
+
+class Resource(models.Model):
+    title = models.CharField(max_length=200, null=False, unique=True)
+    description = models.TextField(null=True, blank=True)
+    resource_url = models.URLField(blank=True, null=True, unique=True)
 
     def __str__(self):
         return self.title
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,10 +26,16 @@ Including another URLconf
 # ]
 
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Serve all API endpoints under /api/
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
